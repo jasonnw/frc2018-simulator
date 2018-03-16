94a12bde2d6d112996209716edd0636f7046e49b
jasonnw <jasonnw@users.noreply.github.com>
2018-03-16 00:34:29 -0700

    Updated eclipse project file

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -5,18 +5,28 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
 ../alliance.cpp \
+../displayPlatform.cpp \
 ../platform.cpp \
 ../robot.cpp \
 ../simulator.cpp 
 
+O_SRCS += \
+../alliance.o \
+../displayPlatform.o \
+../platform.o \
+../robot.o \
+../simulator.o 
+
 OBJS += \
 ./alliance.o \
+./displayPlatform.o \
 ./platform.o \
 ./robot.o \
 ./simulator.o 
 
 CPP_DEPS += \
 ./alliance.d \
+./displayPlatform.d \
 ./platform.d \
 ./robot.d \
 ./simulator.d 
@@ -26,7 +36,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
